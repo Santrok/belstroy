74478299662d94e7cb9c07ee967c7865500719d2
Santrok <a.povedaiko@yandex.ru>
2024-06-14 13:51:40 +0300

    2

--- main/views.py
+++ main/views.py
@@ -6,7 +6,8 @@ from main.models import BackgroundSliderImage, SocialMedia, Contact
 def get_main_page(request):
     slider_photo = BackgroundSliderImage.objects.all()
     social_account = SocialMedia.objects.all()
-    contact = Contact.objects.filter()[1:]
+    contact = Contact.objects.first()
+
 
     context = {
         "slider_photo": slider_photo,
