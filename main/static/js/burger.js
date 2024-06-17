82ae7863c60793fa61eb8e2ecb17762724a49248
Oliver1ck <artem131103@yandex.ru>
2024-06-17 20:13:31 +0300

    fix

--- main/static/js/burger.js
+++ main/static/js/burger.js
@@ -1,7 +1,15 @@
 const burgerBtn = document.querySelector('.main__nav-burger')
 const mainNavList = document.querySelector('.main__nav-list-wrapper');
+const mainNavListItem = document.querySelectorAll('.main__nav-list-item')
 
 burgerBtn.addEventListener('click', () => {
     mainNavList.classList.toggle('main__nav-active')
     burgerBtn.classList.toggle('main__nav-burger-active')
+})
+
+mainNavListItem.forEach(item => {
+    item.addEventListener('click', () => {
+        mainNavList.classList.remove('main__nav-active')
+        burgerBtn.classList.remove('main__nav-burger-active')
+    })
 })
